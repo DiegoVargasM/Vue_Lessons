6aa7ef11f2bb8bf1bab7182ef667688a6759bd6b
DiegoVargasM <diegovargasmiranda@gmail.com>
2023-10-16 22:20:26 -0500

    Create vue app,  data template

---
+++ L1-Creating_a_Vue_app/app.js
@@ -0,0 +1,22 @@
+// create the app
+// the object passed to createApp() represents the root component
+// we can set up data or functions that will be used in the app
+// we also set up the template that will be injected into the DOM
+// we can also set the template directly in the HTML file
+// inside the template we can output dynamic data
+const app = Vue.createApp({
+  // data function
+  data() {
+    return {
+      title: "The Final Empire",
+      author: "Brandon Sanderson",
+      age: 45,
+    };
+  },
+  // functions
+  //template: "<h2>I am the template</h2>",
+});
+
+// mount the app in the DOM element where id="app"
+// (the part of the HTML page that will be managed by Vue)
+app.mount("#app");
