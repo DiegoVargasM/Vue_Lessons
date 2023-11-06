84191d0dd12e002703c76ba507e59ae4a8fe7dfa
DiegoVargasM <diegovargasmiranda@gmail.com>
2023-11-06 16:55:43 -0500

    Improve comments

--- L1-Creating_a_Vue_app/app.js
+++ L1-Creating_a_Vue_app/app.js
@@ -1,8 +1,8 @@
-// create the app
-// the object passed to createApp() represents the root component
+// create the Vue app
+// {} represents the root component
 // we can set up data or functions that will be used in the app
-// we also set up the template that will be injected into the DOM
-// we can also set the template directly in the HTML file
+// we can set up the template that will be injected into the DOM
+// (we can also set the template directly in the HTML file)
 // inside the template we can output dynamic data
 const app = Vue.createApp({
   // data function
