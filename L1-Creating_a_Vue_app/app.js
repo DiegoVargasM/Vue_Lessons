e592d99cd0e912f707b34c1decb213f63fd00604
DiegoVargasM <diegovargasmiranda@gmail.com>
2023-11-06 21:07:24 -0500

    Add isFav toggle functionality

--- L1-Creating_a_Vue_app/app.js
+++ L1-Creating_a_Vue_app/app.js
@@ -54,6 +54,9 @@ const app = Vue.createApp({
       this.x = e.offsetX;
       this.y = e.offsetY;
     },
+    toggleFav(book) {
+      book.isFav = !book.isFav;
+    },
   },
   //template: "<h2>I am the template</h2>",
 });
