7c46a41a1e300742b0d6475b5cd0ee4328d25d64
DiegoVargasM <diegovargasmiranda@gmail.com>
2023-11-13 16:05:00 -0500

    Add props to JobDetails route and link to JobDetails page from Jobs page

--- L4-Vue-Router/ninja-jobs/src/views/jobs/JobDetails.vue
+++ L4-Vue-Router/ninja-jobs/src/views/jobs/JobDetails.vue
@@ -1,10 +1,17 @@
 <template>
-  <div></div>
+  <h1>Job Details Page</h1>
+  <p>The job id is {{ id }}</p>
 </template>
 
 <script>
 export default {
+  props: ["id"],
   name: "JobDetails",
+  data() {
+    return {
+      // id: this.$route.params.id (we have props now)
+    };
+  },
 };
 </script>
 
