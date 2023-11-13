d1cc924fc57465669f45ce4ad43beecab511001a
DiegoVargasM <diegovargasmiranda@gmail.com>
2023-11-13 12:42:58 -0500

    Add Lorem Ipsum text to About and Home pages

--- L4-Vue-Router/ninja-jobs/src/views/About.vue
+++ L4-Vue-Router/ninja-jobs/src/views/About.vue
@@ -1,5 +1,28 @@
 <template>
   <div class="about">
     <h1>This is an about page</h1>
+    <p>
+      Lorem ipsum dolor sit amet consectetur adipisicing elit. Vero explicabo quam
+      blanditiis ipsa qui et doloremque sint laboriosam error modi. Id magnam dolorum cum
+      aut libero esse, porro velit aspernatur! Sapiente blanditiis beatae reprehenderit
+      quis amet numquam, voluptatibus facilis architecto in placeat incidunt earum vel
+      praesentium? Dolorem saepe quia deserunt dicta. Quis sint quidem quia ratione
+      laborum corrupti modi explicabo, similique numquam. Nisi neque dolore voluptates
+      ducimus nam similique iure, inventore possimus aliquam quod illo sit qui veritatis
+      asperiores vero unde odio esse debitis exercitationem. At, nobis soluta. Tempora
+      alias, doloremque nisi enim repellendus ad corporis autem suscipit libero.
+    </p>
+    <p>
+      Lorem ipsum dolor sit amet, consectetur adipisicing elit. Et consequuntur laboriosam
+      natus obcaecati, repellat iste accusantium dolores sint esse eos? Explicabo vero
+      veniam dolorem voluptatum deleniti inventore quibusdam ipsa, quasi recusandae nam,
+      aut nostrum eaque placeat ex similique laboriosam quidem molestiae molestias modi,
+      tempore sequi? Obcaecati in quia laboriosam veritatis ab nemo, quidem numquam cumque
+      incidunt excepturi ea expedita, omnis beatae voluptatibus rem sed perspiciatis at
+      odit facere, consequuntur vero! Ipsam corrupti atque dignissimos voluptates
+      necessitatibus molestias. Repellendus quisquam mollitia exercitationem totam sequi
+      quia amet inventore odio accusamus nostrum doloremque modi esse, natus odit eius
+      fugit. Deleniti nihil enim illo?
+    </p>
   </div>
 </template>
--- L4-Vue-Router/ninja-jobs/src/views/Home.vue
+++ L4-Vue-Router/ninja-jobs/src/views/Home.vue
@@ -1,6 +1,29 @@
 <template>
   <div class="home">
     <h1>This is a home page</h1>
+    <p>
+      Lorem ipsum dolor sit amet consectetur adipisicing elit. Magni, laudantium.
+      Nesciunt, dignissimos. Repudiandae corrupti laboriosam maxime pariatur sapiente
+      dolores! Voluptates, unde quod, quas porro inventore optio illo, fugiat voluptas
+      odio quasi vel! Consequatur perspiciatis autem fuga quas tempore culpa non, soluta
+      perferendis mollitia. Iure, perspiciatis dolore ea amet voluptatum accusamus esse
+      quibusdam, ad vero soluta laudantium tempore consequatur pariatur eaque animi porro
+      expedita, a repellendus explicabo voluptas. Exercitationem a nam quibusdam sint,
+      quod iusto voluptatem, dolorem est voluptate enim optio placeat, non unde iste sunt.
+      In debitis dignissimos, perspiciatis ad consequuntur obcaecati, accusamus non vero
+      animi accusantium ea aliquid nihil!
+    </p>
+    <p>
+      Lorem ipsum dolor sit amet consectetur adipisicing elit. Ipsam natus et corrupti ab
+      exercitationem distinctio ad illo, cum possimus quo voluptate dolore, vel iure quos
+      consequatur saepe provident voluptatem consectetur quam. Est alias id nemo aliquid
+      nulla? Distinctio excepturi deserunt dolores totam, aut error cupiditate fugiat
+      porro quaerat mollitia, nostrum commodi eum! Illum nesciunt consequuntur, deserunt,
+      voluptate sint voluptates hic, quas velit dolorem eum inventore minus ipsam
+      laboriosam dolores quasi atque. Quo quos minima pariatur hic accusamus possimus
+      culpa? Id, illo veniam quis debitis molestiae ea magni impedit dolores possimus?
+      Deleniti at hic atque. Rerum iure eveniet quibusdam magni veniam.
+    </p>
   </div>
 </template>
 
