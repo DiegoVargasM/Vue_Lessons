58b66e59b5696ff0ac834a0fc9861ebfe779f48f
DiegoVargasM <diegovargasmiranda@gmail.com>
2023-11-21 13:42:07 -0500

    Create Home view using Composition API

--- L5-Composition-API/dojo-blog/src/views/Home.vue
+++ L5-Composition-API/dojo-blog/src/views/Home.vue
@@ -1,10 +1,33 @@
 <template>
-  <div class="home"></div>
+  <div class="home">
+    Home
+    <!-- Second, we assign the ref property  -->
+    <p ref="p">My name is {{ name }} and my age is {{ age }}</p>
+    <button @click="handleClick">Click me</button>
+  </div>
 </template>
 
 <script>
+import { ref } from "vue";
+
 export default {
   name: "Home",
-  components: {},
+  setup() {
+    console.log("setup");
+
+    let name = "mario";
+    let age = 30;
+
+    // First, we need to create a reference to the element we want to target
+    const p = ref(null);
+
+    const handleClick = () => {
+      console.log(p, p.value);
+      p.value.classList.add("test");
+      p.value.textContent = "Hello, ninjas";
+    };
+
+    return { name, age, handleClick, p };
+  },
 };
 </script>
