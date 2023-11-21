bbb924e88f064406fe1bc9b8effbed34d0c3e6c9
DiegoVargasM <diegovargasmiranda@gmail.com>
2023-11-21 16:41:33 -0500

    Create view and show names array

--- L5-Composition-API/dojo-blog/src/views/ComputedValues.vue
+++ L5-Composition-API/dojo-blog/src/views/ComputedValues.vue
@@ -1,11 +1,23 @@
-<template></template>
+<template>
+  <div class="home">
+    <h1>Computed Values</h1>
+    <div v-for="name in names" :key="name">
+      {{ name }}
+    </div>
+  </div>
+</template>
 
 <script>
+import { ref, computed } from "vue";
+
 export default {
   name: "ComputedValues",
+  setup() {
+    const names = ref(["mario", "yoshi", "luigi", "toad", "bowser", "koopa", "peach"]);
+
+    const name = computed(() => {});
+
+    return { names };
+  },
 };
 </script>
-
-<style scoped>
-/* Add your component styles here */
-</style>
