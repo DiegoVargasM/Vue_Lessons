7b125cf33db498ab018169ed9f47427527f1d988
DiegoVargasM <diegovargasmiranda@gmail.com>
2023-11-21 16:05:57 -0500

    Update router and add Home2 component

---
+++ L5-Composition-API/dojo-blog/src/views/Home2.vue
@@ -0,0 +1,25 @@
+<template>
+  <div class="home">
+    <h1>Home 2</h1>
+    <h2>Refs</h2>
+    <p>{{ ninjaOne.name }} - {{ ninjaOne.age }}</p>
+    <button @click="updateNinjaOne">Update Ninja One</button>
+  </div>
+</template>
+
+<script>
+import { ref, reactive } from "vue";
+
+export default {
+  name: "Home2",
+  setup() {
+    const ninjaOne = ref({ name: "Pepe", age: 30 });
+
+    const updateNinjaOne = () => {
+      ninjaOne.value.age = 40;
+    };
+
+    return { ninjaOne, updateNinjaOne };
+  },
+};
+</script>
