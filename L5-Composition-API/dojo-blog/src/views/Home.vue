7c32b37a4713f3ebf7c864e4657b616b02bd2253
DiegoVargasM <diegovargasmiranda@gmail.com>
2023-11-22 18:34:54 -0500

    Add PostList component to Home view

--- L5-Composition-API/dojo-blog/src/views/Home.vue
+++ L5-Composition-API/dojo-blog/src/views/Home.vue
@@ -1,14 +1,17 @@
 <template>
   <div class="home">
     <h1>Welcome to the Home page!</h1>
+    <PostList :posts="posts" />
   </div>
 </template>
 
 <script>
+import PostList from "../components/PostList.vue";
 import { ref } from "vue";
 
 export default {
   name: "Home",
+  components: { PostList },
   setup() {
     const posts = ref([
       { title: "My first post", body: "Lorem ipsum.", id: 1 },
