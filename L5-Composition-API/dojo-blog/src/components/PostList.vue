265d277cb3fb1fa3904429fd53fcc7b0424b5435
DiegoVargasM <diegovargasmiranda@gmail.com>
2023-11-22 18:37:44 -0500

    Add SinglePost component to PostList

--- L5-Composition-API/dojo-blog/src/components/PostList.vue
+++ L5-Composition-API/dojo-blog/src/components/PostList.vue
@@ -1,15 +1,18 @@
 <template>
   <div class="post-list">
     <div v-for="post in posts" :key="post.id">
-      <h3>{{ post.title }}</h3>
+      <SinglePost :post="post" />
     </div>
   </div>
 </template>
 
 <script>
+import SinglePost from "./SinglePost.vue";
+
 export default {
   name: "PostList",
   props: ["posts"],
+  components: { SinglePost },
   setup(props) {
     console.log(props.posts);
   },
---
+++ L5-Composition-API/dojo-blog/src/components/SinglePost.vue
@@ -0,0 +1,15 @@
+<template>
+  <div class="single-post">
+    <h2>{{ post.title }}</h2>
+    <p>{{ post.body }}</p>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "SinglePost",
+  props: ["post"],
+};
+</script>
+
+<style scoped></style>
