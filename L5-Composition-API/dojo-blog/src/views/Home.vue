fef2eaf074a2f1f727bb4dcc4655f11ccf995977
DiegoVargasM <diegovargasmiranda@gmail.com>
2023-11-22 22:08:59 -0500

    Delete buttons and hardcoded data

--- L5-Composition-API/dojo-blog/src/views/Home.vue
+++ L5-Composition-API/dojo-blog/src/views/Home.vue
@@ -1,9 +1,7 @@
 <template>
   <div class="home">
     <h1>Welcome to the Home page!</h1>
-    <PostList v-if="showPosts" :posts="posts" />
-    <button @click="showPosts = !showPosts">Toggle posts</button>
-    <button @click="posts.pop()">Delete a post</button>
+    <PostList :posts="posts" />
   </div>
 </template>
 
@@ -15,24 +13,10 @@ export default {
   name: "Home",
   components: { PostList },
   setup() {
-    const posts = ref([
-      {
-        title: "My first post",
-        body:
-          "Lorem ipsum dolor sit amet consectetur adipiscing elit platea sociis, ligula penatibus velit sagittis lacus felis nam ad, vel malesuada tempor vulputate parturient phasellus porttitor fringilla. Vestibulum torquent at tristique potenti purus montes ultrices elementum quisque, senectus dictum sed condimentum pretium habitasse taciti nullam nulla per, nibh est urna mollis nunc facilisi lobortis praesent. Class lacinia libero metus in curae natoque iaculis pellentesque cursus, aliquam cum blandit ullamcorper tempus venenatis pulvinar hac, justo nascetur mus congue dictumst placerat odio massa. Augue convallis quam interdum sociosqu laoreet eros arcu ridiculus, sodales tincidunt mauris mi non morbi nec enim gravida, imperdiet lectus netus bibendum scelerisque mattis accumsan. A fames litora egestas luctus ultricies dis duis rutrum, hendrerit orci himenaeos maecenas magnis quis etiam suspendisse, semper volutpat erat posuere fermentum auctor euismod. Rhoncus ante conubia proin commodo aptent ornare sollicitudin, feugiat faucibus dignissim facilisis fusce porta eleifend, ut et eu nostra consequat diam.",
-        id: 1,
-      },
-      {
-        title: "My second post",
-        body:
-          "Lorem ipsum dolor sit amet consectetur adipiscing elit platea sociis, ligula penatibus velit sagittis lacus felis nam ad, vel malesuada tempor vulputate parturient phasellus porttitor fringilla. Vestibulum torquent at tristique potenti purus montes ultrices elementum quisque, senectus dictum sed condimentum pretium habitasse taciti nullam nulla per, nibh est urna mollis nunc facilisi lobortis praesent. Class lacinia libero metus in curae natoque iaculis pellentesque cursus, aliquam cum blandit ullamcorper tempus venenatis pulvinar hac, justo nascetur mus congue dictumst placerat odio massa. Augue convallis quam interdum sociosqu laoreet eros arcu ridiculus, sodales tincidunt mauris mi non morbi nec enim gravida, imperdiet lectus netus bibendum scelerisque mattis accumsan. A fames litora egestas luctus ultricies dis duis rutrum, hendrerit orci himenaeos maecenas magnis quis etiam suspendisse, semper volutpat erat posuere fermentum auctor euismod. Rhoncus ante conubia proin commodo aptent ornare sollicitudin, feugiat faucibus dignissim facilisis fusce porta eleifend, ut et eu nostra consequat diam.",
-        id: 2,
-      },
-    ]);
+    const posts = ref([]);
+    const error = ref(null);
 
-    const showPosts = ref(true);
-
-    return { posts, showPosts };
+    return { posts };
   },
 };
 </script>
