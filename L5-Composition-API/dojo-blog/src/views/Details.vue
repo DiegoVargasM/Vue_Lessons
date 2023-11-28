d1ce30cb421a2b49fe5b511128b6b813326be8b3
DiegoVargasM <diegovargasmiranda@gmail.com>
2023-11-28 13:47:37 -0500

    Add Spinner comp to Details view

--- L5-Composition-API/dojo-blog/src/views/Details.vue
+++ L5-Composition-API/dojo-blog/src/views/Details.vue
@@ -4,13 +4,20 @@
     <h3>{{ post.title }}</h3>
     <p class="pre">{{ post.body }}</p>
   </div>
+  <div v-else>
+    <Spinner />
+  </div>
 </template>
 <script>
 import getPost from "../composables/getPost";
+import Spinner from "../components/Spinner.vue";
 
 export default {
   name: "Details",
   props: ["id"],
+  components: {
+    Spinner,
+  },
   setup(props) {
     const { post, error, load } = getPost(props.id);
 
