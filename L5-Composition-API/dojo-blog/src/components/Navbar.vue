42dfa4e246c9516dcd4e48004eac38e9828bc7c2
DiegoVargasM <diegovargasmiranda@gmail.com>
2023-12-04 08:52:09 -0500

    Add Navbar component

---
+++ L5-Composition-API/dojo-blog/src/components/Navbar.vue
@@ -0,0 +1,17 @@
+<template>
+  <header>
+    <h1>The Dojo Blog</h1>
+    <nav>
+      <router-link :to="{ name: 'Home' }">Home</router-link>
+      <router-link :to="{ name: 'Create' }">Create a Post</router-link>
+    </nav>
+  </header>
+</template>
+
+<script>
+export default {
+  name: "Navbar",
+};
+</script>
+
+<style></style>
