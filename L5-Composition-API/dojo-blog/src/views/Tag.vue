435057326b4e463561bfa650a7cf6e58752a7fea
DiegoVargasM <diegovargasmiranda@gmail.com>
2023-12-04 12:18:20 -0500

    Fetch posts using composable

--- L5-Composition-API/dojo-blog/src/views/Tag.vue
+++ L5-Composition-API/dojo-blog/src/views/Tag.vue
@@ -1,9 +1,25 @@
 <template>
-  <div class="tag"></div>
+  <div class="tag">
+    <h1>tags</h1>
+  </div>
 </template>
 
 <script>
-export default {};
+import getPosts from "../composables/getPosts";
+import Spinner from "../components/Spinner.vue";
+
+export default {
+  name: "Tag",
+  components: { Spinner },
+  setup() {
+    const { posts, error, load } = getPosts();
+
+    load();
+    console.log(posts);
+
+    return { posts, error };
+  },
+};
 </script>
 
 <style></style>
