9aad3c42546161287d81e20b7f4dab7dd9cac9b1
DiegoVargasM <diegovargasmiranda@gmail.com>
2023-12-04 12:30:01 -0500

    Add post filtering functionality to Tag.vue

--- L5-Composition-API/dojo-blog/src/views/Tag.vue
+++ L5-Composition-API/dojo-blog/src/views/Tag.vue
@@ -7,15 +7,26 @@
 <script>
 import getPosts from "../composables/getPosts";
 import Spinner from "../components/Spinner.vue";
+import { useRoute } from "vue-router";
+import { computed } from "vue";
 
 export default {
   name: "Tag",
   components: { Spinner },
   setup() {
     const { posts, error, load } = getPosts();
-
     load();
-    console.log(posts);
+
+    const route = useRoute();
+    const tag = route.params.tag;
+    console.log(tag, "tag");
+
+    //array of posts that have the tag
+    const tagPosts = computed(() => {
+      return posts.value.filter((post) => post.tags.includes(tag));
+    });
+
+    console.log(tagPosts, "tagPosts");
 
     return { posts, error };
   },
